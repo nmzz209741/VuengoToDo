25840902a2f547b997e6674a2eb9c14cf195b716
shreeti <shreetis13@gmail.com>
2020-08-23 23:06:39 +0545

    add new task done

--- todo_front/src/views/Home.vue
+++ todo_front/src/views/Home.vue
@@ -3,72 +3,66 @@
     <nav class="navbar navbar-light bg-light">
       <span class="navbar-brand mb-0 h1">Vuengo ToDo App</span>
     </nav>
-    <div class="row">
-      <div class="col-md-3">
-        <form>
-          <h2 class="subtitle">Add Task</h2>
-          <div class="form-group">
-            <label for="title">Title</label>
-            <input type="text" class="input form-control"
-              placeholder="Make a to do list :D" name="title">
-          </div>
-          <div class="form-group">
-            <label for="status">Status</label>
-            <select name="status" id="status" class="form-control">
-              <option value="todo">To Do</option>
-              <option value="in_progress">In Progress</option>
-              <option value="done">Done</option>
-            </select>
-          </div>
-          <div class="form-group">
-            <button class='btn btn-sm btn-primary' type=submit>Add Task</button>
-          </div>
-        </form>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <form @submit.prevent="addTask">
+            <h4 class="subtitle">Add Task</h4>
+            <div class="row">
+              <div class="col-md-10">
+                <div class="form-group">
+                  <input
+                    type="text"
+                    class="input form-control"
+                    placeholder="Make a to do list :D"
+                    name="title"
+                    v-model="title"
+                  />
+                </div>
+              </div>
+              <div class="col-md-2">
+                <div class="form-group">
+                  <button class="btn btn-sm btn-primary" type="submit">Add Task</button>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
-    <div class="row">
-      <div class="col-md-4">
-        <h2 class="subtitle">
-          To Do
-        </h2>
-        <div class="todo">
-          <div class="card"
-            v-for="task in todoTasks"
-            :key="task.id">
-            <div class="card-content">{{ task.title }}</div>
-            <footer class="card-footer">
-              <a class="card-footer-item">In Progress</a>
-            </footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <h4 class="subtitle">To Do</h4>
+          <div class="todo">
+            <div class="card" v-for="task in todoTasks" :key="task.id">
+              <div class="card-content">{{ task.title }}</div>
+              <footer class="card-footer">
+                <a class="card-footer-item">In Progress</a>
+              </footer>
+            </div>
           </div>
         </div>
-      </div>
-      <div class="col-md-4">
-        <h2 class="subtitle">
-          In Progress
-        </h2>
-        <div class="in_progress">
-          <div class="card"
-            v-for="task in inProgressTasks"
-            :key="task.id">
-            <div class="card-content">{{task.title}}</div>
-            <footer class="card-footer">
-              <a href="" class="card-footer-item">Done</a>
-            </footer>
+        <div class="col-md-4">
+          <h4 class="subtitle">In Progress</h4>
+          <div class="in_progress">
+            <div class="card" v-for="task in inProgressTasks" :key="task.id">
+              <div class="card-content">{{task.title}}</div>
+              <footer class="card-footer">
+                <a href class="card-footer-item">Done</a>
+              </footer>
+            </div>
           </div>
         </div>
-      </div>
-      <div class="col-md-4">
-        <h2 class="subtitle">
-          Done
-        </h2>
-        <div class="done">
-          <div class="card"
-            v-for="task in doneTasks"
-            :key="task.id">
-            <div class="card-content">{{task.title}}</div>
-            <footer class="card-footer">
-              <a href="" class="card-footer-item">Delete</a>
-            </footer>
+        <div class="col-md-4">
+          <h4 class="subtitle">Done</h4>
+          <div class="done">
+            <div class="card" v-for="task in doneTasks" :key="task.id">
+              <div class="card-content">{{task.title}}</div>
+              <footer class="card-footer">
+                <a href class="card-footer-item">Delete</a>
+              </footer>
+            </div>
           </div>
         </div>
       </div>
@@ -76,47 +70,83 @@
   </div>
 </template>
 <script>
-const BASE_URL = 'http://127.0.0.1:8000/'
-import axios from 'axios';
+const BASE_URL = "http://127.0.0.1:8000";
+const AUTH = {
+  username: "admin",
+  password: "admin"
+};
+import axios from "axios";
 export default {
-  name: 'Home',
+  name: "Home",
   data() {
     return {
       tasks: [],
+      title: null,
+      status: "todo",
+      errors: []
     };
   },
   created() {
-    this.getTasks()
+    this.getTasks();
   },
   methods: {
-    getTasks: function () {
-      var vm = this
-      axios.get('http://127.0.0.1:8000/tasks/', {
-        auth: {
-          username: 'admin',
-          password: 'admin'
-        }
-      })
-      .then(response => {
-        console.log(response)
-        vm.tasks = response.data
-      })
+    getTasks: function() {
+      var vm = this;
+      axios
+        .get(`${BASE_URL}/tasks/`, {
+          auth: AUTH
+        })
+        .then(response => {
+          console.log(response);
+          vm.tasks = response.data;
+        });
+    },
+    addTask: function() {
+      if (this.title) {
+        axios({
+          method: "post",
+          url: BASE_URL + "/tasks/",
+          data: {
+            title: this.title,
+            status: "todo"
+          },
+          auth: {
+            username: "admin",
+            password: "admin"
+          }
+        })
+          .then(response => {
+            let newTask = {
+              id: response.data.id,
+              title: this.title,
+              status: "todo"
+            };
+            this.tasks.push(newTask);
+            this.title = "";
+            this.status = "todo";
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      } else {
+        if (!this.title) this.errors.push("Title is required");
+      }
     }
   },
   computed: {
     todoTasks() {
-      return this.tasks.filter(task=> task.status === 'todo')
+      return this.tasks.filter(task => task.status === "todo");
     },
     inProgressTasks() {
-      return this.tasks.filter(task=> task.status === 'in_progress')
+      return this.tasks.filter(task => task.status === "in_progress");
     },
     doneTasks() {
-      return this.tasks.filter(task=> task.status === 'done')
+      return this.tasks.filter(task => task.status === "done");
     }
   }
 };
 </script>
 
 <style>
-@import './../assets/css/home.scss';
+@import "./../assets/css/home.scss";
 </style>
